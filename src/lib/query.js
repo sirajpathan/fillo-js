dcd10f3890596c3bc695357466be2efacf2b9f0a
unknown <pathan.siraj@hotmail.com>
2020-04-15 23:09:38 +0530

    lint issue fixed

--- src/lib/query.js
+++ src/lib/query.js
@@ -8,7 +8,7 @@ export function query (db, str, params) {
         case "update":
             return insertOrUpdate(db, str, params);
         case "delete":
-                return insertOrUpdate(db, str, params);
+            return insertOrUpdate(db, str, params);
         default:
             return Promise.reject("Incorrect query");
     }
